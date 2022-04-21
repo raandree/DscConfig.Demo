f6eb19e9820974e6f608539358504488be534fcd
Raimund Andree <r.andree@live.com>
2022-04-21 12:24:48 +0200

    Finalized Pester 5 tests

--- tests/Unit/DSCResources/DscResources.Tests.ps1
+++ tests/Unit/DSCResources/DscResources.Tests.ps1
@@ -1,5 +1,5 @@
 BeforeDiscovery {
-    $dscResources = Get-DscResource -Module DscConfig.Demo
+    $dscResources = Get-DscResource -Module $moduleUnderTest.Name
     $here = $PSScriptRoot
 
     $skippedDscResources = 'PowerShellRepositories'
@@ -28,11 +28,32 @@ BeforeDiscovery {
         }
     }
 
+    $compositeResources = Get-DscResource -Module $moduleUnderTest.Name
+    $finalTestCases = @()
+    $finalTestCases += @{
+        AllCompositeResources      = $compositeResources.Name
+        FilteredCompositeResources = $compositeResources | Where-Object Name -NotIn $skippedDscResources
+        AllCompositeResourceFolders   = dir -Path "$($moduleUnderTest.ModuleBase)\DSCResources\*"
+        FilteredCompositeResourceFolders   = dir -Path "$($moduleUnderTest.ModuleBase)\DSCResources\*" | Where-Object BaseName -NotIn $skippedDscResources
+    }
 }
 
 Describe 'DSC Composite Resources compile' -Tags FunctionalQuality {
+    BeforeAll {
+        $tempExists = Test-Path -Path C:\Temp
+        if (-not $tempExists)
+        {
+            New-Item -Path C:\Temp -ItemType Directory | Out-Null
+        }
+        @'
+function f1 {
+Get-Date
+}
+f1
+'@ | Set-Content -Path C:\Temp\JeaRoleTest.ps1 -Force
+    }
 
-    It "'<DscResourceName>' compiles"-TestCases $testCases {
+    It "'<DscResourceName>' compiles" -TestCases $testCases {
 
         if ($Skip)
         {
@@ -46,21 +67,26 @@ Describe 'DSC Composite Resources compile' -Tags FunctionalQuality {
         }
         $configurationData.AllNodes = @($nodeData)
 
-        configuration "Config_$dscResourceName" {
+        $dscConfiguration = @'
+configuration "Config_$dscResourceName" {
 
-            Import-DscResource -ModuleName DscConfig.Demo
+    #<importStatements>
 
-            node "localhost_$dscResourceName" {
+    node "localhost_$dscResourceName" {
 
-                $data = $configurationData.Datum.Config."$dscResourceName"
-                if (-not $data)
-                {
-                    $data = @{}
-                }
-
-                (Get-DscSplattedResource -ResourceName $dscResourceName -ExecutionName $dscResourceName -Properties $data -NoInvoke).Invoke($data)
-            }
+        $data = $configurationData.Datum.Config."$dscResourceName"
+        if (-not $data)
+        {
+            $data = @{}
         }
+
+        (Get-DscSplattedResource -ResourceName $dscResourceName -ExecutionName $dscResourceName -Properties $data -NoInvoke).Invoke($data)
+    }
+}
+'@
+
+        $dscConfiguration = $dscConfiguration.Replace('#<importStatements>', "Import-DscResource -Module $($moduleUnderTest.Name)")
+        Invoke-Expression -Command $dscConfiguration
 
         {
             & "Config_$dscResourceName" -ConfigurationData $configurationData -OutputPath $OutputDirectory -ErrorAction Stop
@@ -68,7 +94,8 @@ Describe 'DSC Composite Resources compile' -Tags FunctionalQuality {
     }
 
     It "'<DscResourceName>' should have created a mof file" -TestCases $testCases {
-        if ($DscResourceName -in $skippedDscResources)
+
+        if ($Skip)
         {
             Set-ItResult -Skipped -Because "Tests for '$DscResourceName' are skipped"
         }
@@ -76,34 +103,36 @@ Describe 'DSC Composite Resources compile' -Tags FunctionalQuality {
         $mofFile = Get-Item -Path "$($OutputDirectory)\localhost_$DscResourceName.mof" -ErrorAction SilentlyContinue
         $mofFile | Should -BeOfType System.IO.FileInfo
     }
+
+    AfterAll {
+        Remove-Item -Path C:\Temp\JeaRoleTest.ps1
+        if (-not $tempExists)
+        {
+            Remove-Item -Path C:\Temp
+        }
+    }
 }
 
 Describe 'Final tests' -Tags FunctionalQuality {
-    BeforeAll {
-        $compositeResouces = Get-DscResource -Module DscConfig.Demo
-        Write-Host "Number of composite resources: $($compositeResouces.Count)"
-        $compositeResouces = $compositeResouces | Where-Object Name -NotIn $skippedDscResources
-        Write-Host "Number of composite resources (considering 'skippedDscResources'): $($compositeResouces.Count)"
-    }
 
-    It 'Every composite resource has compiled' {
+    It 'Every composite resource has compiled' -TestCases $finalTestCases {
 
         $mofFiles = Get-ChildItem -Path $OutputDirectory -Filter *.mof
         Write-Host "Number of compiled MOF files: $($mofFiles.Count)"
-        $compositeResouces.Count | Should -Be $mofFiles.Count
+        $FilteredCompositeResources.Count | Should -Be $mofFiles.Count
 
     }
 
-    It 'Composite resource folder count matches composite resource count' {
+    It 'Composite resource folder count matches composite resource count' -TestCases $finalTestCases {
 
-        $compositeResouceFolders = dir -Path "$($moduleUnderTest.ModuleBase)\DSCResources\*"
-        Write-Host "Number of composite resource folders: $($compositeResouceFolders.Count)"
-        $compositeResouceFolders = $compositeResouceFolders | Where-Object Name -NotIn $skippedDscResources
-        Write-Host "Number of composite resource folders (considering 'skippedDscResources'): $($compositeResouceFolders.Count)"
-        Write-Host "Number of composite resources: $($compositeResouces.Count)"
-        Write-Host (Compare-Object -ReferenceObject $compositeResouceFolders.Name -DifferenceObject $compositeResouces.Name | Out-String) -ForegroundColor Yellow
+        Write-Host "Number of composite resource folders: $($AllCompositeResourceFolders.Count)"
+        Write-Host "Number of composite resource folders (considering 'skippedDscResources'): $($FilteredCompositeResourceFolders.Count)"
+        Write-Host "Number of all composite resources: $($AllCompositeResources.Count)"
+        Write-Host "Number of composite resources (considering 'skippedDscResources'): $($FilteredCompositeResources.Count)"
 
-        $compositeResouces.Count | Should -Be $compositeResouceFolders.Count
+        Write-Host (Compare-Object -ReferenceObject $FilteredCompositeResourceFolders.BaseName -DifferenceObject $FilteredCompositeResources.Name | Out-String) -ForegroundColor Yellow
+
+        $FilteredCompositeResourceFolders.Count | Should -Be $FilteredCompositeResources.Count
 
     }
 }
